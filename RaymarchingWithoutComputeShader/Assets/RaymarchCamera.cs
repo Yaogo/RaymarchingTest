bd8c7c151a6bb0b4c2c39700de2cbc5b8dcf8cef
yaohua <572707834@qq.com>
2020-06-18 23:07:34 +0800

    init

---
+++ RaymarchingWithoutComputeShader/Assets/RaymarchCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public class RaymarchCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Shader mShader;
+    private Material mRaymarchMat;
+    public Material mRaymarchMaterial
+    {
+        get
+        {
+            if (!mRaymarchMat && mShader)
+            {
+                mRaymarchMat = new Material(mShader);
+                mRaymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return mRaymarchMat;
+        }
+    }
+    private Camera mCam;
+    public Camera mCamera
+    {
+        get
+        {
+            if (!mCam)
+                mCam = GetComponent<Camera>();
+            return mCam;
+        }
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (!mRaymarchMat)
+        {
+            Graphics.Blit(source, destination);
+                return;
+        }
+    }
+
+    private Matrix4x4 CamFrustum(Camera cam)
+    {
+        return Matrix4x4.identity;
+    }
+}
